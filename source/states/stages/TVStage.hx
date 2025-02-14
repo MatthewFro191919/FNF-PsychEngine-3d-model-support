8226345f80229d15de4a8c8d66db38660f8e8a0a
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-14 02:57:48 -0500

    Update TVStage.hx

--- source/states/stages/TVStage.hx
+++ source/states/stages/TVStage.hx
@@ -323,4 +323,14 @@ class TVStage extends BaseStage
 
 		prop.nodes[ParticleRotateToPositionNode.POSITION_VECTOR3D] = new Vector3D(Math.random() * 40 - 20, Math.random() * 40 - 20, Math.random() * 40 - 20);
 	}
+
+	function startCharacterPos(char:Character, ?gfCheck:Bool = false) {
+		if(gfCheck && char.curCharacter.startsWith('gf')) { //IF DAD IS GIRLFRIEND, HE GOES TO HER POSITION
+			char.setPosition(GF_X, GF_Y);
+			char.scrollFactor.set(0.95, 0.95);
+			char.danceEveryNumBeats = 2;
+		}
+		char.x += char.positionArray[0];
+		char.y += char.positionArray[1];
+	}
 }
